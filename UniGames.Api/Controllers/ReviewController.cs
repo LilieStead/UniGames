86b763a5b55e9486db07aff777c87203b20f51ca
Benjamin7288 <Benjamin7288@outlook.com>
2023-10-31 19:29:12 +0000

    unfinished
    Left to do:
    Need to create two CRUD options in the api. 1st is to search for a review by gameID and userID to be used in javascript. 2nd is to update said review.
    
    Final part of the js that need to be done to find the review by GameID and UserID to get the ReviewID, show it to user for editing then submitting it.

--- UniGames.Api/Controllers/ReviewController.cs
+++ UniGames.Api/Controllers/ReviewController.cs
@@ -97,6 +97,8 @@ namespace UniGames.Api.Controllers
             return Ok(reviewDTO);
         }
 
+        
+
     }
 }
 
---
+++ UniGames.UI/assets/js/editreview.js
@@ -0,0 +1,85 @@
+function searchReview(event){
+    event.preventDefault();
+
+    // Get UserID by Username
+    fetch(`http://localhost:5116/user/${username}`)
+        .then(response => {
+            if (response.status === 200) {
+                console.log("User authenticated");
+
+                return response.json();
+            } else if (response.status === 404) {
+                // User is not found
+                window.location.href = "error.html?error=2";
+            } else {
+                console.error("error", response.status);
+            }
+        })
+        .then(data => {
+            const userID = data.userId;
+        }
+    
+     // Get GameID by Game Title
+    ,fetch(`http://localhost:5116/game/${title}`)
+        .then(response => {
+             if (response.status === 200) {
+                 console.log("User authenticated");
+
+                 return response.json();
+             } else if (response.status === 404) {
+                 // Game is not found
+                 window.location.href = "error.html?error=2";
+             } else {
+                 console.error("error", response.status);
+             }
+        })
+        .then(data => {
+             const userID = data.gameId;
+        }
+
+    // Find the review by GameID and UserID to get the ReviewID
+
+   ,fetch('http://localhost:5116/review/')
+
+}    
+
+// The JS for updateing the review
+function editReview(event){
+    event.preventDefault();
+
+    const formData = new FormData(document.getElementById("editreview"));
+
+    const reviewTitle = formData.get("ReviewTitle");
+    const reviewDesc = formData.get("ReviewDescription");
+    const score = formData.get("Score");
+
+    
+    data.ReviewTitle = reviewTitle;
+    data.ReviewDescription = reviewDesc;
+    data.Score = score;
+    data.UserID = userID;
+    data.GameID = gameID;
+
+
+            fetch('http://localhost:5116/review/', {
+                method: "",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("API Response: ", data)
+                window.location.href = "assets/inc/success.html?success=1";
+            })
+
+            .catch(error => {
+                console.error("Error:", error);
+            });
+            
+}
+
+
+document.getElementById("searchreview").addEventListener("submit", searchReview);
+document.getElementById("editreview").addEventListener("submit", editReview);
