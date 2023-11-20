95418e5148bc80f5ae78db05be1284f4acf8c1dd
Kizzer <kieronbrown11@hotmail.com>
2023-11-20 01:14:13 +0000

    Added Username Display
    The username displays on the where the sidemenu is and updates per user (as in it shows the correct username for the user that is logged in)

--- UniGames.UI/assets/js/sidemenu.js
+++ UniGames.UI/assets/js/sidemenu.js
@@ -1,3 +1,28 @@
+function getUsername(){
+  const userIDSess = sessionStorage.getItem('authToken');
+  const userIDLocal = localStorage.getItem('authTokenLocal');
+  const apiURL = userIDSess
+  ? `http://localhost:5116/user/decodeToken?jwtToken=${userIDSess}`
+  : userIDLocal
+  ?`http://localhost:5116/user/decodeToken?jwtToken=${userIDLocal}`
+  : null;
+  fetch(apiURL)
+  .then(response => response.json())
+  .then(data => {
+    const username = data.username;
+
+    const findUserElement = document.getElementById('userNameSide');
+
+    findUserElement.innerHTML = username;
+  })
+  .catch(error => {
+    console.error("error:" , error);
+  })
+}
+
+
+
+
 function toggleMenu() {
   var arrow = document.getElementById("menuarrow")
     var menu = document.getElementById("hamburgerMenu");
@@ -11,3 +36,4 @@ function toggleMenu() {
     }
   }
   
+getUsername();
