ae47b5211edb419257dd4e350770f782c4d2f063
Kizzer <kieronbrown11@hotmail.com>
2023-11-25 15:36:14 +0000

    save

--- UniGames.Api/Models/DTOs/UpdateGameDTO.cs
+++ UniGames.Api/Models/DTOs/UpdateGameDTO.cs
@@ -1,12 +1,15 @@
-﻿using UniGames.Api.Models.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using UniGames.Api.Models.Domain;
 
 namespace UniGames.Api.Models.DTOs
 {
     public class UpdateGameDTO
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int PlatformID { get; set; }
-
+        [Required]
         public int UserID { get; set; }
 
     }
